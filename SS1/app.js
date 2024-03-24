5dd5994d5df487fd6dd829685135860bfd1bd54f
hieu1966hn <hieu1966hn@gmail.com>
2024-03-24 17:36:18 +0700

    add đề bài ss1

--- SS1/app.js
+++ SS1/app.js
@@ -18,5 +18,7 @@ Yêu cầu:
 - Sử dụng push() để thêm 1 học sinh mới vào
 - Sử dụng find() hoặc findIndex() đề tìm kiếm học sinh theo id.
 - Sử dụng splice() để xóa 1 học sinh khỏi mảng dựa trên id.
+- Tạo 1 function cho phép tìm kiếm học sinh dựa trên tên (name) hoặc chuyên ngành (major. 
+Function này trả về mảng mới chứa các học sinh phù hợp với điều kiện)
 - Cập nhật và in ra mảng mới.
 */
